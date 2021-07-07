8c8f3b801c9c607094e6314d1f768978d5cff638
viniciustristao <ti.viniciust@gmail.com>
2021-07-07 10:03:33 -0300

    Alterando Pacote

--- src/main/java/api/project/curso/person/person/api/PersonApiApplication.java
+++ src/main/java/api/project/curso/person/person/api/PersonApiApplication.java
@@ -1,4 +1,4 @@
-package api.project.curso.person.PersonAPI;
+package api.project.curso.person.person.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/api/project/curso/person/person/api/controller/PersonController.java
+++ src/main/java/api/project/curso/person/person/api/controller/PersonController.java
@@ -1,4 +1,4 @@
-package api.project.curso.person.PersonAPI.controller;
+package api.project.curso.person.person.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
